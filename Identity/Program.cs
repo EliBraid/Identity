39728acdbd23408c05de15f51ff36667219b4ffa
怡超 王 <w1632050253@outlook.com>
2022-11-18 19:08:33 +0800

    test 1

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Identity
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var service = new ServiceCollection();
+
+            
         }
     }
 }

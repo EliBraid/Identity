743355f8d8d18820b5beb93e6c9c7f05b6e2b66e
怡超 王 <w1632050253@outlook.com>
2022-11-22 10:28:58 +0800

    Init

---
+++ CleanDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CleanDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("1.txt");
+
+            foreach (var line in lines)
+            {
+                string[] segments = line.Split('|');
+
+                string email = segments[0];
+                string title = segments[1];
+                string body = segments[2];
+
+                Console.WriteLine($"发送邮件: {email},{title},{body}");
+            }
+        }
+    }
+}
